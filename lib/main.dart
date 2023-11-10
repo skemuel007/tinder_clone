399539b12e1d52af1eac8bb0eec03b74beff73bd
Salvation Kemuel <skemuel007>
2023-11-10 23:09:15 +0100

    Add decrement button

--- lib/main.dart
+++ lib/main.dart
@@ -16,6 +16,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData.dark().copyWith(
         scaffoldBackgroundColor: Colors.black
       ),
+      debugShowCheckedModeBanner: false,
       /* ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
